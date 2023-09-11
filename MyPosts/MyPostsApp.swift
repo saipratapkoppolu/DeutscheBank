1c43385de375f4ebb6886daf2ad0f24eea3efd9f
Sai Pratap Koppolu <sai.koppolu@gmail.com>
2023-09-11 18:58:27 +0200

    Initial Commit

---
+++ MyPosts/MyPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPostsApp.swift
+//  MyPosts
+//
+//  Created by Sai Pratap Koppolu on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
